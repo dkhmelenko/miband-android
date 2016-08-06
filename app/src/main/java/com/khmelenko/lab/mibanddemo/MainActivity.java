519ff69108cfb7737dfe17fbd148bf5a4ad2c64f
Dmytro Khmelenko <dmitriy-broker@rambler.ru>
2016-08-06 18:50:30 +0200

    Replace anonymous function with lambdas in test app

--- app/src/main/java/com/khmelenko/lab/mibanddemo/MainActivity.java
+++ app/src/main/java/com/khmelenko/lab/mibanddemo/MainActivity.java
@@ -9,17 +9,11 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.khmelenko.lab.miband.MiBand;
-import com.khmelenko.lab.miband.listeners.HeartRateNotifyListener;
-import com.khmelenko.lab.miband.listeners.NotifyListener;
-import com.khmelenko.lab.miband.listeners.RealtimeStepsNotifyListener;
 import com.khmelenko.lab.miband.model.BatteryInfo;
 import com.khmelenko.lab.miband.model.LedColor;
 import com.khmelenko.lab.miband.model.UserInfo;
@@ -31,7 +25,6 @@ import java.util.Arrays;
 
 import rx.Subscriber;
 import rx.Subscription;
-import rx.functions.Action1;
 
 /**
  * Main application activity
@@ -94,175 +87,129 @@ public class MainActivity extends Activity {
         miband = new MiBand(this);
         ListView lv = (ListView) findViewById(R.id.listView);
         lv.setAdapter(new ArrayAdapter<>(this, R.layout.item, BUTTONS));
-        lv.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                int menuIndex = 0;
-                if (position == menuIndex++) {
-                    final ProgressDialog pd = ProgressDialog.show(MainActivity.this, "", "Connecting...");
-                    rx.Observable<Boolean> observable = miband.connect(device);
-                    Subscription subscription = observable.subscribe(new Subscriber<Boolean>() {
-                        @Override
-                        public void onCompleted() {
+        lv.setOnItemClickListener((parent, view, position, id) -> {
+            int menuIndex = 0;
+            if (position == menuIndex++) {
+                final ProgressDialog pd = ProgressDialog.show(MainActivity.this, "", "Connecting...");
+                rx.Observable<Boolean> observable = miband.connect(device);
+                Subscription subscription = observable.subscribe(new Subscriber<Boolean>() {
+                    @Override
+                    public void onCompleted() {
 
-                            Log.d(TAG, "Connect onCompleted");
-                        }
+                        Log.d(TAG, "Connect onCompleted");
+                    }
 
-                        @Override
-                        public void onError(Throwable e) {
-                            pd.dismiss();
-                            e.printStackTrace();
-                        }
+                    @Override
+                    public void onError(Throwable e) {
+                        pd.dismiss();
+                        e.printStackTrace();
+                    }
 
-                        @Override
-                        public void onNext(Boolean result) {
-                            pd.dismiss();
-                            Log.d(TAG, "Connect onNext: " + String.valueOf(result));
-                        }
-                    });
-                } else if (position == menuIndex++) {
-                    // TODO miband.showServicesAndCharacteristics();
-                } else if (position == menuIndex++) {
-                    rx.Observable<Integer> observable = miband.readRssi();
-                    observable.subscribe(new Subscriber<Integer>() {
-                        @Override
-                        public void onCompleted() {
-                            Log.d(TAG, "Rssi onCompleted");
-                        }
+                    @Override
+                    public void onNext(Boolean result) {
+                        pd.dismiss();
+                        Log.d(TAG, "Connect onNext: " + String.valueOf(result));
+                    }
+                });
+            } else if (position == menuIndex++) {
+                // TODO miband.showServicesAndCharacteristics();
+            } else if (position == menuIndex++) {
+                rx.Observable<Integer> observable = miband.readRssi();
+                observable.subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d(TAG, "Rssi onCompleted");
+                    }
 
-                        @Override
-                        public void onError(Throwable e) {
-                            Log.d(TAG, "readRssi fail");
-                        }
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "readRssi fail");
+                    }
 
-                        @Override
-                        public void onNext(Integer rssi) {
-                            Log.d(TAG, "rssi:" + String.valueOf(rssi));
-                        }
-                    });
-                } else if (position == menuIndex++) {
-                    rx.Observable<BatteryInfo> observable = miband.getBatteryInfo();
-                    observable.subscribe(new Action1<BatteryInfo>() {
-                        @Override
-                        public void call(BatteryInfo batteryInfo) {
-                            Log.d(TAG, batteryInfo.toString());
-                        }
-                    }, new Action1<Throwable>() {
-                        @Override
-                        public void call(Throwable throwable) {
-                            Log.d(TAG, "getBatteryInfo fail");
-                        }
-                    });
-                } else if (position == menuIndex++) {
-                    UserInfo userInfo = new UserInfo(20271234, 1, 32, 160, 40, "alias", 0);
-                    Log.d(TAG, "setUserInfo:" + userInfo.toString() + ",data:" + Arrays.toString(userInfo.getBytes(miband.getDevice().getAddress())));
-                    miband.setUserInfo(userInfo);
-                } else if (position == menuIndex++) {
+                    @Override
+                    public void onNext(Integer rssi) {
+                        Log.d(TAG, "rssi:" + String.valueOf(rssi));
+                    }
+                });
+            } else if (position == menuIndex++) {
+                rx.Observable<BatteryInfo> observable = miband.getBatteryInfo();
+                observable.subscribe(batteryInfo -> {
+                    Log.d(TAG, batteryInfo.toString());
+                }, throwable -> {
+                    Log.d(TAG, "getBatteryInfo fail");
+                });
+            } else if (position == menuIndex++) {
+                UserInfo userInfo = new UserInfo(20271234, 1, 32, 160, 40, "alias", 0);
+                Log.d(TAG, "setUserInfo:" + userInfo.toString() + ",data:" + Arrays.toString(userInfo.getBytes(miband.getDevice().getAddress())));
+                miband.setUserInfo(userInfo);
+            } else if (position == menuIndex++) {
 
-                    miband.setHeartRateScanListener(new HeartRateNotifyListener() {
-                        @Override
-                        public void onNotify(int heartRate) {
-                            Log.d(TAG, "heart rate: " + heartRate);
-                        }
-                    });
-                } else if (position == menuIndex++) {
-                    miband.startHeartRateScan();
-                } else if (position == menuIndex++) {
-                    miband.startVibration(VibrationMode.VIBRATION_WITH_LED).subscribe(new Action1<Void>() {
-                        @Override
-                        public void call(Void aVoid) {
-                            Log.d(TAG, "Vibration started");
-                        }
-                    });
-                } else if (position == menuIndex++) {
-                    miband.startVibration(VibrationMode.VIBRATION_WITHOUT_LED).subscribe(new Action1<Void>() {
-                        @Override
-                        public void call(Void aVoid) {
-                            Log.d(TAG, "Vibration started");
-                        }
-                    });
-                    ;
-                } else if (position == menuIndex++) {
-                    miband.startVibration(VibrationMode.VIBRATION_10_TIMES_WITH_LED).subscribe(new Action1<Void>() {
-                        @Override
-                        public void call(Void aVoid) {
-                            Log.d(TAG, "Vibration started");
-                        }
-                    });
-                    ;
-                } else if (position == menuIndex++) {
-                    miband.stopVibration().subscribe(new Action1<Void>() {
-                        @Override
-                        public void call(Void aVoid) {
-                            Log.d(TAG, "Vibration stopped");
-                        }
-                    });
-                    ;
-                } else if (position == menuIndex++) {
-                    miband.setNormalNotifyListener(new NotifyListener() {
+                miband.setHeartRateScanListener(heartRate -> Log.d(TAG, "heart rate: " + heartRate));
+            } else if (position == menuIndex++) {
+                miband.startHeartRateScan();
+            } else if (position == menuIndex++) {
+                miband.startVibration(VibrationMode.VIBRATION_WITH_LED).subscribe(aVoid -> {
+                    Log.d(TAG, "Vibration started");
+                });
+            } else if (position == menuIndex++) {
+                miband.startVibration(VibrationMode.VIBRATION_WITHOUT_LED).subscribe(aVoid -> {
+                    Log.d(TAG, "Vibration started");
+                });
 
-                        @Override
-                        public void onNotify(byte[] data) {
-                            Log.d(TAG, "NormalNotifyListener:" + Arrays.toString(data));
-                        }
-                    });
-                } else if (position == menuIndex++) {
-                    miband.setRealtimeStepsNotifyListener(new RealtimeStepsNotifyListener() {
+            } else if (position == menuIndex++) {
+                miband.startVibration(VibrationMode.VIBRATION_10_TIMES_WITH_LED).subscribe(aVoid -> {
+                    Log.d(TAG, "Vibration started");
+                });
 
-                        @Override
-                        public void onNotify(int steps) {
-                            Log.d(TAG, "RealtimeStepsNotifyListener:" + steps);
-                        }
-                    });
-                } else if (position == menuIndex++) {
-                    miband.enableRealtimeStepsNotify();
-                } else if (position == menuIndex++) {
-                    miband.disableRealtimeStepsNotify();
-                } else if (position == menuIndex++) {
-                    miband.setLedColor(LedColor.ORANGE);
-                } else if (position == menuIndex++) {
-                    miband.setLedColor(LedColor.BLUE);
-                } else if (position == menuIndex++) {
-                    miband.setLedColor(LedColor.RED);
-                } else if (position == menuIndex++) {
-                    miband.setLedColor(LedColor.GREEN);
-                } else if (position == menuIndex++) {
-                    miband.setSensorDataNotifyListener(new NotifyListener() {
-                        @Override
-                        public void onNotify(byte[] data) {
-                            ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
-                            int i = 0;
+            } else if (position == menuIndex++) {
+                miband.stopVibration().subscribe(aVoid -> {
+                    Log.d(TAG, "Vibration stopped");
+                });
 
-                            int index = (data[i++] & 0xFF) | (data[i++] & 0xFF) << 8;
-                            int d1 = (data[i++] & 0xFF) | (data[i++] & 0xFF) << 8;
-                            int d2 = (data[i++] & 0xFF) | (data[i++] & 0xFF) << 8;
-                            int d3 = (data[i++] & 0xFF) | (data[i++] & 0xFF) << 8;
+            } else if (position == menuIndex++) {
+                miband.setNormalNotifyListener(data -> Log.d(TAG, "NormalNotifyListener:" + Arrays.toString(data)));
+            } else if (position == menuIndex++) {
+                miband.setRealtimeStepsNotifyListener(steps -> Log.d(TAG, "RealtimeStepsNotifyListener:" + steps));
+            } else if (position == menuIndex++) {
+                miband.enableRealtimeStepsNotify();
+            } else if (position == menuIndex++) {
+                miband.disableRealtimeStepsNotify();
+            } else if (position == menuIndex++) {
+                miband.setLedColor(LedColor.ORANGE);
+            } else if (position == menuIndex++) {
+                miband.setLedColor(LedColor.BLUE);
+            } else if (position == menuIndex++) {
+                miband.setLedColor(LedColor.RED);
+            } else if (position == menuIndex++) {
+                miband.setLedColor(LedColor.GREEN);
+            } else if (position == menuIndex++) {
+                miband.setSensorDataNotifyListener(data -> {
+                    ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+                    int i = 0;
 
-                            Message m = new Message();
-                            m.what = Message_What_ShowLog;
-                            m.obj = index + "," + d1 + "," + d2 + "," + d3;
+                    int index = (data[i++] & 0xFF) | (data[i++] & 0xFF) << 8;
+                    int d1 = (data[i++] & 0xFF) | (data[i++] & 0xFF) << 8;
+                    int d2 = (data[i++] & 0xFF) | (data[i++] & 0xFF) << 8;
+                    int d3 = (data[i++] & 0xFF) | (data[i++] & 0xFF) << 8;
 
-                            handler.sendMessage(m);
-                        }
-                    });
-                } else if (position == menuIndex++) {
-                    miband.enableSensorDataNotify();
-                } else if (position == menuIndex++) {
-                    miband.disableSensorDataNotify();
-                } else if (position == menuIndex++) {
-                    rx.Observable<Void> observable = miband.pair();
-                    observable.subscribe(new Action1<Void>() {
-                        @Override
-                        public void call(Void aVoid) {
-                            Log.d(TAG, "Pairing successful");
-                        }
-                    }, new Action1<Throwable>() {
-                        @Override
-                        public void call(Throwable throwable) {
-                            Log.d(TAG, "Pairing failed");
-                            throwable.printStackTrace();
-                        }
-                    });
-                }
+                    Message m = new Message();
+                    m.what = Message_What_ShowLog;
+                    m.obj = index + "," + d1 + "," + d2 + "," + d3;
+
+                    handler.sendMessage(m);
+                });
+            } else if (position == menuIndex++) {
+                miband.enableSensorDataNotify();
+            } else if (position == menuIndex++) {
+                miband.disableSensorDataNotify();
+            } else if (position == menuIndex++) {
+                rx.Observable<Void> observable = miband.pair();
+                observable.subscribe(aVoid -> {
+                    Log.d(TAG, "Pairing successful");
+                }, throwable -> {
+                    Log.d(TAG, "Pairing failed");
+                    throwable.printStackTrace();
+                });
             }
         });
 
