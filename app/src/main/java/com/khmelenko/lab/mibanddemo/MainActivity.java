07165c522114ba2327c632f5f478294dd4dacdb8
Dmytro Khmelenko <dmitriy-broker@rambler.ru>
2016-11-19 15:28:53 +0100

    Improve sample app

--- app/src/main/java/com/khmelenko/lab/mibanddemo/MainActivity.java
+++ app/src/main/java/com/khmelenko/lab/mibanddemo/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.khmelenko.lab.miband.MiBand;
-import com.khmelenko.lab.miband.model.BatteryInfo;
 import com.khmelenko.lab.miband.model.UserInfo;
 import com.khmelenko.lab.miband.model.VibrationMode;
 
@@ -17,8 +16,6 @@ import java.nio.ByteOrder;
 import java.util.Arrays;
 
 import butterknife.OnClick;
-import io.reactivex.Observable;
-import io.reactivex.disposables.Disposable;
 
 /**
  * Main application activity
@@ -45,20 +42,19 @@ public class MainActivity extends AppCompatActivity {
         final BluetoothDevice device = intent.getParcelableExtra("device");
 
         final ProgressDialog pd = ProgressDialog.show(MainActivity.this, "", "Connecting...");
-        Observable<Boolean> observable = mMiBand.connect(device);
-        Disposable subscription = observable.subscribe(result -> {
-            pd.dismiss();
-            Log.d(TAG, "Connect onNext: " + String.valueOf(result));
-        }, throwable -> {
-            pd.dismiss();
-            throwable.printStackTrace();
-        });
+        mMiBand.connect(device)
+                .subscribe(result -> {
+                    pd.dismiss();
+                    Log.d(TAG, "Connect onNext: " + String.valueOf(result));
+                }, throwable -> {
+                    pd.dismiss();
+                    throwable.printStackTrace();
+                });
     }
 
     @OnClick(R.id.action_pair)
     public void actionPair() {
-        Observable<Void> observable = mMiBand.pair();
-        observable.subscribe(aVoid -> {
+        mMiBand.pair().subscribe(aVoid -> {
             Log.d(TAG, "Pairing successful");
         }, throwable -> {
             Log.d(TAG, "Pairing failed");
@@ -73,27 +69,32 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.action_read_rssi)
     public void actionReadRssi() {
-        Observable<Integer> observable = mMiBand.readRssi();
-        observable.subscribe(rssi -> Log.d(TAG, "rssi:" + String.valueOf(rssi)),
-                throwable -> Log.d(TAG, "readRssi fail"),
-                () -> Log.d(TAG, "Rssi onCompleted"));
+        mMiBand.readRssi()
+                .subscribe(rssi -> Log.d(TAG, "rssi:" + String.valueOf(rssi)),
+                        throwable -> Log.d(TAG, "readRssi fail"),
+                        () -> Log.d(TAG, "Rssi onCompleted"));
     }
 
     @OnClick(R.id.action_battery_info)
     public void actionBatteryInfo() {
-        Observable<BatteryInfo> observable = mMiBand.getBatteryInfo();
-        observable.subscribe(batteryInfo -> {
-            Log.d(TAG, batteryInfo.toString());
-        }, throwable -> {
-            Log.d(TAG, "getBatteryInfo fail");
-        });
+        mMiBand.getBatteryInfo()
+                .subscribe(batteryInfo -> {
+                    Log.d(TAG, batteryInfo.toString());
+                }, throwable -> {
+                    Log.d(TAG, "getBatteryInfo fail");
+                });
     }
 
     @OnClick(R.id.action_set_user_info)
     public void actionSetUserInfo() {
         UserInfo userInfo = new UserInfo(20271234, 1, 32, 160, 40, "alias", 0);
         Log.d(TAG, "setUserInfo:" + userInfo.toString() + ",data:" + Arrays.toString(userInfo.getBytes(mMiBand.getDevice().getAddress())));
-        mMiBand.setUserInfo(userInfo);
+        mMiBand.setUserInfo(userInfo)
+                .subscribe(aVoid -> {
+                    // do nothing
+                }, throwable -> {
+                    Log.d(TAG, "setUserInfo failed");
+                });
     }
 
     @OnClick(R.id.action_set_heart_rate_notify_listener)
@@ -108,30 +109,34 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.action_start_vibro_with_led)
     public void actionStartVibroWithLed() {
-        mMiBand.startVibration(VibrationMode.VIBRATION_WITH_LED).subscribe(Void -> {
-            Log.d(TAG, "Vibration started");
-        });
+        mMiBand.startVibration(VibrationMode.VIBRATION_WITH_LED)
+                .subscribe(Void -> {
+                    Log.d(TAG, "Vibration started");
+                });
     }
 
     @OnClick(R.id.action_start_vibro)
     public void actionStartVibro() {
-        mMiBand.startVibration(VibrationMode.VIBRATION_WITHOUT_LED).subscribe(Void -> {
-            Log.d(TAG, "Vibration started");
-        });
+        mMiBand.startVibration(VibrationMode.VIBRATION_WITHOUT_LED)
+                .subscribe(Void -> {
+                    Log.d(TAG, "Vibration started");
+                });
     }
 
     @OnClick(R.id.action_start_vibro_with_led_time)
     public void actionStartVibroWithLedAndTime() {
-        mMiBand.startVibration(VibrationMode.VIBRATION_10_TIMES_WITH_LED).subscribe(Void -> {
-            Log.d(TAG, "Vibration started");
-        });
+        mMiBand.startVibration(VibrationMode.VIBRATION_10_TIMES_WITH_LED)
+                .subscribe(Void -> {
+                    Log.d(TAG, "Vibration started");
+                });
     }
 
     @OnClick(R.id.action_stop_vibro)
     public void actionStopVibration() {
-        mMiBand.stopVibration().subscribe(Void -> {
-            Log.d(TAG, "Vibration stopped");
-        });
+        mMiBand.stopVibration()
+                .subscribe(Void -> {
+                    Log.d(TAG, "Vibration stopped");
+                });
     }
 
     @OnClick(R.id.action_set_notify_listener)
