7ef838eb3f5beb91f35876dbb93345f1689e5f9f
Dmytro Khmelenko <dmitriy-broker@rambler.ru>
2017-07-09 23:28:30 +0200

    Convert BluetoothListener to Kotlin

---
+++ miband-sdk-kotlin/src/main/java/com/khmelenko/lab/miband/BluetoothListener.kt
@@ -0,0 +1,53 @@
+package com.khmelenko.lab.miband
+
+import android.bluetooth.BluetoothGattCharacteristic
+import java.util.*
+
+/**
+ * Bluetooth listener
+ *
+ * @author Dmytro Khmelenko (d.khmelenko@gmail.com)
+ */
+interface BluetoothListener {
+
+    /**
+     * Called on established connection
+     */
+    fun onConnectionEstablished()
+
+    /**
+     * Called on disconnection
+     */
+    fun onDisconnected()
+
+    /**
+     * Called on getting successful result
+     *
+     * @param data Characteristic data
+     */
+    fun onResult(data: BluetoothGattCharacteristic)
+
+    /**
+     * Called on getting successful result of RSSI strength
+     *
+     * @param rssi RSSI strength
+     */
+    fun onResultRssi(rssi: Int)
+
+    /**
+     * Called on fail from service
+     *
+     * @param serviceUUID      Service UUID
+     * @param characteristicId Characteristic ID
+     * @param msg              Error message
+     */
+    fun onFail(serviceUUID: UUID, characteristicId: UUID, msg: String)
+
+    /**
+     * Called on fail from Bluetooth IO
+     *
+     * @param errorCode Error code
+     * @param msg       Error message
+     */
+    fun onFail(errorCode: Int, msg: String)
+}
